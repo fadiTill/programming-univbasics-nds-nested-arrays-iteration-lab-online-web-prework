2f5c56d5e449a887d5d4420f839e9c5f5283fd32
fadimata tillman <fadi.tillman@gmail.com>
2020-01-07 20:00:25 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,6 +1,9 @@
 def join_ingredients(src)
   # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
   #
+  join_ingredients(src) = [ [Apples,pears],[strawsbery,rasberry],[lemeon,orange]
+  ]
+  
   # Build a new Array that contains strings where each pair of foods is
   # inserted into this template:
   #
